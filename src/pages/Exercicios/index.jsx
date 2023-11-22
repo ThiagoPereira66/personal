41fe5f298a2655e82486b2b332b8b02a2db310b8
ThiagoPereira66 <thiagopereirasantos66@gmail.com>
2023-11-22 13:05:25 -0300

    conteudo da pagina de exercicios

--- src/pages/Exercicios/index.jsx
+++ src/pages/Exercicios/index.jsx
@@ -1,8 +1,32 @@
+import Peito from "../../../public/Img/peito.png"
+import Abdomen from "../../../public/Img/abdomen.png"
+import Costas from "../../../public/Img/costas.png"
+import Gluteos from "../../../public/Img/gluteos.png"
+import Perna from "../../../public/Img/pernas.png"
+import Triceps from "../../../public/Img/triceps.png"
+import Poster from "../../../public/Img/Personal Trainer  Instrutor Fitness.png"
+import Corrida from "../../../public/Img/corrida.png"
+
+
+import { Banner, Card, Container, Img } from "./style"
+
+
+
 function Exercicios(){
     return(
-        <>
-        <h1>Exercícios</h1>
-        </>
+        <Container>
+           <Banner src={Poster} alt=""/> 
+        <Card>
+            <Img src={Peito} alt="" />
+            <Img src={Costas} alt="" />
+            <Img src={Triceps} alt="" />
+            <Img src={Abdomen} alt="" />
+            <Img src={Gluteos} alt="" />
+            <Img src={Perna} alt="" />     
+        </Card>
+            <Banner src={Corrida} />
+        
+        </Container>
     )
 }
 
---
+++ src/pages/Exercicios/style.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+background-color: silver;
+`
+
+export const Card = styled.div`
+width: 100%;
+position: relative;
+padding: .5rem;
+display: flex;
+flex-wrap: wrap;
+justify-content: space-between;
+
+`
+
+export const Img = styled.img`
+width: 14rem;
+border-radius: .5rem;
+margin-bottom: 2rem;
+box-shadow: 0 0 7px black;
+
+`
+
+export const Banner = styled.img`
+width: 100%;
+margin: .5rem 0;
+`
